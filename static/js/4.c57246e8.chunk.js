1bc91bd17e700e7367490d71c88c3699c2ba2d78
weiq <6682220aabc@163.com>
2018-09-20 14:50:21 +0800

    Updates

--- static/js/4.c57246e8.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{475:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s=n(502);n.n(s);t.default={namespace:"home",subscriptions:{setup:function(e){var t=e.history,n=e.dispatch;return t.listen(function(e){"/home"===e.pathname&&n({type:"global/getMenu"})})}},state:{},effects:{},reducers:{}}},502:function(e,t){}});
-//# sourceMappingURL=4.c57246e8.chunk.js.map
